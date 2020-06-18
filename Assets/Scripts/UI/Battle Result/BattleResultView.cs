14317cd74877e6aae985cf9bfdafca729bc5770b
Mustafa Pamir Ünsal <pamir.unsal@gmail.com>
2020-06-18 21:15:29 +0300

    Result Panel

---
+++ Assets/Scripts/UI/Battle Result/BattleResultView.cs
@@ -0,0 +1,28 @@
+﻿using IoC_Container;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Battle_Result
+{
+    public class BattleResultView : MonoBehaviour
+    {
+        [SerializeField] private Text textResult;
+        
+        private GameManager gameManager;
+
+        private void Awake()
+        {
+            gameManager = Container.Get<GameManager>();
+        }
+
+        private void OnEnable()
+        {
+            if (gameManager == null)
+            {
+                gameManager = Container.Get<GameManager>();
+            }
+
+            textResult.text = gameManager.whoWon;
+        }
+    }
+}
